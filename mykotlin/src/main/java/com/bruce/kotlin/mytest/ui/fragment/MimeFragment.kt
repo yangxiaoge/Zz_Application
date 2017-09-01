9bb02202ea0c1250e117439af0a090755b14c077
yang.jianan <364804051@qq.com>
2017-09-01 17:48:03 +0800

    add Kotlin 仿开眼 四个tab页白板

---
+++ mykotlin/src/main/java/com/bruce/kotlin/mytest/ui/fragment/MimeFragment.kt
@@ -0,0 +1,19 @@
+package com.bruce.kotlin.mytest.ui.fragment
+
+import com.bruce.kotlin.mytest.R
+import kotlinx.android.synthetic.main.home_fragment.*
+
+/**
+ * Created by yang.jianan on 2017/09/01 17:07.
+ */
+
+class MimeFragment : BaseFragment() {
+    override fun initViews() {
+        txt_content.text = "Mime"
+    }
+
+    override fun getLayoutResources(): Int {
+        return R.layout.home_fragment;
+    }
+
+}
