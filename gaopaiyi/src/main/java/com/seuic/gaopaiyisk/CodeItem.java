73d94d7eebbb7daf9dbc60655444e550a9efb7cc
yangjianan <364804051@qq.com>
2018-06-08 12:19:48 +0800

    开始展暂停功能实现，以及实时条目展示

---
+++ gaopaiyi/src/main/java/com/seuic/gaopaiyisk/CodeItem.java
@@ -0,0 +1,12 @@
+package com.seuic.gaopaiyisk;
+
+/**
+ * Created by yangjianan on 2018/6/8.
+ */
+class CodeItem {
+    public String barcode;
+
+    public CodeItem(String barcode) {
+        this.barcode = barcode;
+    }
+}
--- gaopaiyi/src/main/java/com/seuic/gaopaiyisk/MainActivity.java
+++ gaopaiyi/src/main/java/com/seuic/gaopaiyisk/MainActivity.java
@@ -3,12 +3,20 @@ package com.seuic.gaopaiyisk;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.fsa.decoder.SymbologyID;
 import com.seuic.bean.CommodityInfo;
 import com.seuic.callback.CompleteCallback;
@@ -20,42 +28,85 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements CompleteCallback {
+public class MainActivity extends AppCompatActivity implements CompleteCallback, View.OnClickListener {
     private SurfaceView mSurfaceView;
     private HSIScanner hsiScanner;
     private Socket mClientSocket;
     private PrintWriter mPrintWriter;
+    private ImageView mIvPlay; //启动开关
+    private boolean isOpened; //是否已经开启
+    private List<CodeItem> data;
+    private RecyclerView codeListRV;
+    private CodeListAdapter codeListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initData();
 
-        //初始化sdk
-        hsiScanner = HSIScanner.getInstance(this, mSurfaceView);
-        hsiScanner.setCompleteCallback(this);
+        startScanner();
+    }
+
+
+
+    private void initData() {
+        initRV();
+    }
+
+    private void initRV() {
+        data = new ArrayList<>();
+        codeListAdapter = new CodeListAdapter(data);
+        codeListRV.setLayoutManager(new LinearLayoutManager(this));
+        codeListRV.setAdapter(codeListAdapter);
     }
 
     private void initView() {
         mSurfaceView = findViewById(R.id.surfaceview);
+        codeListRV = findViewById(R.id.rv_list);
+        mIvPlay = findViewById(R.id.iv_play);
+        mIvPlay.setOnClickListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //打开和设置参数, 这个可以放在 按钮中执行
+        Log.e("MainActivity","onResume");
+      /*  //打开和设置参数
         hsiScanner.open();
-        hsiScanner.setParams(SymbologyID.CODE128, 1); // 1，表示打开该条码，0 表示关闭
+        //hsiScanner.setParams(SymbologyID.CODE128, 1); // 1，表示打开该条码，0 表示关闭
+        hsiScanner.setParams(SymbologyID.QR, 0); // 1，表示打开该条码，0 表示关闭*/
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.e("MainActivity","onPause");
         //onDestory中关闭释放对象
-        hsiScanner.close();
-        HSIScanner.destroyInstance();
+        closeScanner();
+    }
+
+    /**
+     * 初始化HSIScanner 对象
+     */
+    private void startScanner() {
+        hsiScanner = HSIScanner.getInstance(this, mSurfaceView);
+        hsiScanner.setCompleteCallback(this);
+    }
+
+    /**
+     * 关闭scanner释放对象
+     */
+    private void closeScanner() {
+        if (hsiScanner != null) {
+            hsiScanner.close();
+            HSIScanner.destroyInstance();
+        }
     }
 
     @Override
@@ -73,6 +124,19 @@ public class MainActivity extends AppCompatActivity implements CompleteCallback 
             case R.id.menu_setting:
                 //弹窗设置页面
                 //showSettingDialog();
+
+                if (!isOpened) {
+                    mIvPlay.setImageResource(R.drawable.ic_pause_circle_outline_black_64dp);
+                    isOpened = true;
+                    //打开和设置参数
+                    hsiScanner.open();
+                    hsiScanner.setParams(SymbologyID.CODE128, 1); // 1，表示打开该条码，0 表示关闭
+                    hsiScanner.setParams(SymbologyID.QR, 1); // 1，表示打开该条码，0 表示关闭
+                } else {
+                    mIvPlay.setImageResource(R.drawable.ic_play_circle_outline_black_64dp);
+                    isOpened = false;
+                    closeScanner();
+                }
                 break;
             default:
                 break;
@@ -110,7 +174,7 @@ public class MainActivity extends AppCompatActivity implements CompleteCallback 
             for (int i = 0; i < strings.length; i++) {
                 SeuicLog.d("barcode:" + strings[i]);
 
-                if (mClientSocket != null) {
+                /*if (mClientSocket != null) {
                     if (isServerClose(mClientSocket)) {
                         sendCommodityInfo2Bankend(strings[i]);
                     } else {
@@ -118,34 +182,56 @@ public class MainActivity extends AppCompatActivity implements CompleteCallback 
                     }
                 } else {
                     sendCommodityInfo2Bankend(strings[i]);
-                }
-                /*if (mClientSocket == null || (mClientSocket != null && isServerClose(mClientSocket))) {
-                    sendCommodityInfo2Bankend(strings[i]);
-                }else {
-                    mPrintWriter.println(strings[i]);
                 }*/
+                if (mClientSocket == null || (mClientSocket != null && isServerClose(mClientSocket))) {
+                    // TODO: 2018/6/8 先注释掉
+                    //sendCommodityInfo2Bankend(strings[i]);
+                } else {
+                    sendPrint2Server(strings[i]);
+                }
             }
         }
     }
 
     private void sendCommodityInfo2Bankend(String barcode) {
+        int retryCount = 3; //重连尝试
         Socket socket = null;
-        while (socket == null) {
+        while (socket == null && retryCount > 0) {
             try {
                 //socket = new Socket("192.168.80.64", 7777);
-                socket=new Socket("192.168.1.117",8688);
+                socket = new Socket("192.168.1.117", 8688);
                 mClientSocket = socket;
                 mPrintWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
                 System.out.println("连接服务器成功");
-
                 //发送数据给服务器
-                mPrintWriter.println(barcode);
+                sendPrint2Server(barcode);
             } catch (IOException e) {
+                retryCount--;
                 e.printStackTrace();
                 releaseSocket();
             }
 
         }
+    }
+
+    /**
+     * 发送数据给服务器
+     *
+     * @param barcode barcode
+     */
+    private void sendPrint2Server(String barcode) {
+        //更新code list
+        data.add(new CodeItem(barcode));
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                codeListAdapter.notifyDataSetChanged();
+            }
+        });
+
+        //PrintWriter发给服务器
+        if (mPrintWriter == null) return;
+        mPrintWriter.println(barcode);
     }
 
     /**
@@ -159,6 +245,7 @@ public class MainActivity extends AppCompatActivity implements CompleteCallback 
             socket.sendUrgentData(0);// 发送 1 个字节的紧急数据，默认情况下，服务器端没有开启紧急数据处理，不影响正常通信
             return false;
         } catch (Exception se) {
+            se.printStackTrace();
             return true;
         }
     }
@@ -181,6 +268,43 @@ public class MainActivity extends AppCompatActivity implements CompleteCallback 
                 e.printStackTrace();
             }
             mPrintWriter = null;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_play:
+                if (!isOpened) {
+                    mIvPlay.setImageResource(R.drawable.ic_pause_circle_outline_black_64dp);
+                    isOpened = true;
+                    //打开和设置参数
+                    hsiScanner.open();
+                    //可以设置多个，具体看SymbologyID中
+                    hsiScanner.setParams(SymbologyID.CODE128, 1); // 1，表示打开该条码，0 表示关闭
+                    hsiScanner.setParams(SymbologyID.QR, 1); // 1，表示打开该条码，0 表示关闭
+                } else {
+                    mIvPlay.setImageResource(R.drawable.ic_play_circle_outline_black_64dp);
+                    isOpened = false;
+                    closeScanner();
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    class CodeListAdapter extends BaseQuickAdapter<CodeItem, BaseViewHolder> {
+
+        public CodeListAdapter(@Nullable List<CodeItem> data) {
+            super(data);
+            mLayoutResId = android.R.layout.simple_list_item_1;
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, CodeItem item) {
+            helper.setText(android.R.id.text1, item.barcode);
         }
     }
 }
